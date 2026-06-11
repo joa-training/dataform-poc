489a24722b126926b7fb4faba5fe9656d611b918
Olmos <j.olmos.astudillo@accenture.com>
2026-06-11 16:39:30 -0400

    MOD: raw_banking and std_banking, cst_banking

--- includes/constants.js
+++ includes/constants.js
@@ -3,9 +3,9 @@ module.exports = {
   PROJECT_ID: "test-hackaton-484418",
 
   ORIG_SCHEMA: "std_banking",            // Dataset maestro (Flujo 1)
-  DEST_RAW: "raw_interfaces",            // Dataset interfaz On-Premise (Flujo 2)
-  DEST_STD: "cuentas_std",               // Dataset intermedia
-  DEST_CST: "cuentas_consumption",       // Dataset consumo
+  DEST_RAW: "raw_banking",            // Dataset interfaz On-Premise (Flujo 2)
+  DEST_STD: "std_banking",               // Dataset intermedia
+  DEST_CST: "cst_banking",       // Dataset consumo
 
   // Variables de control
   FECHA_EJE: "2026-04-08"                
