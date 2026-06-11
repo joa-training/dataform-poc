96af1fed027faeaa7169fc87104cf77ac0c3b5cf
Olmos <j.olmos.astudillo@accenture.com>
2026-06-11 11:52:26 -0400

    MOD: add UNION between maestro and on-prem

--- includes/constants.js
+++ includes/constants.js
@@ -1,9 +1,12 @@
 module.exports = {
-  // Esquemas 
-  ORIG_SCHEMA: "dataset_maestros",       // El dataset de tabla de origen
-  DEST_STD: "cuentas_std",               // Dataset para la capa intermedia
-  DEST_CST: "cuentas_consumption",       // Dataset para la capa final de consumo
+
+  PROJECT_ID: "test-hackaton-484418",
+
+  ORIG_SCHEMA: "std_banking",            // Dataset maestro (Flujo 1)
+  DEST_RAW: "raw_interfaces",            // Dataset interfaz On-Premise (Flujo 2)
+  DEST_STD: "cuentas_std",               // Dataset intermedia
+  DEST_CST: "cuentas_consumption",       // Dataset consumo
 
   // Variables de control
-  FECHA_EJE: "2026-04-08"                // Fecha de ejecución para filtros o pruebas
+  FECHA_EJE: "2026-04-08"                
 };
